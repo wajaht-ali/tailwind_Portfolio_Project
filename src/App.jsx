aebc6fd6bef650f20d05003e0c70b79ee3ae2bd4
Wajahat-404 <109886908+Wajahat-404@users.noreply.github.com>
2023-11-20 15:24:30 +0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './styles/App.css';
+
+function App() {
+
+  return (
+    <div className="w-full text-center">
+      TailwindCss Portfolio Template
+    </div>
+  )
+}
+
+export default App
