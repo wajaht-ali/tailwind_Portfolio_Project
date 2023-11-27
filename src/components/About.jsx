abeab4053638f14a55783be7566d970dbb9e4ffe
Wajahat-404 <109886908+Wajahat-404@users.noreply.github.com>
2023-11-27 10:29:46 +0500

    About grid adjustments

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,16 @@
 const About = () => {
     return (
         <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
-            About
+            <div className="w-full h-full flex flex-col justify-center items-center">
+                <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8 px-4">
+                    <div className="sm:text-right pb-8 pl-4">
+                        <p className="text-7xl font-bold inline border-b-4 border-pink-600">About</p>
+                    </div>
+                </div>
+                <div>
+
+                </div>
+            </div>
         </div>
     )
 }
