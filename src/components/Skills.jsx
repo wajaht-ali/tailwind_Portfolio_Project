a4c511ee2ee314bf2efc605e8a30220aa732f63c
Wajahat-404 <109886908+Wajahat-404@users.noreply.github.com>
2023-12-04 14:45:58 +0500

    Work component structuring and designing

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 import HTML from '../assets/images/html.png';
 import CSS from '../assets/images/css.png';
 import JS from '../assets/images/javascript.png';
@@ -16,41 +17,58 @@ const Skills = () => {
       <div className="max-w-[1000px] h-full flex flex-col mx-auto">
         <div className="p-4">
           <h1 className="text-4xl font-bold border-b-4 rounded-sm border-pink-600 inline pt-3">Skills</h1>
-          <p className="text-lg mt-4">These are the technologies I&apos;ve worked with:</p>
+          <p className="text-lg mt-4">//These are the technologies I&apos;ve worked with:</p>
         </div>
 
         <div className="w-full grid grid-cols-2 md:grid-rows-2 md:grid-cols-4 p-4 mx-auto h-full place-items-center">
           <div className='w-[180px] h-[150px] rounded shadow-md shadow-[#040c16] p-2 m-3'>
             <img className='rounded mx-auto w-[80%] h-[80%]' src={HTML} alt="HTML" />
-            <p className='text-lg uppercase text-center my-2 font-bold'>html</p>
+            <p className='text-lg uppercase text-center my-2 font-bold'>HTMl</p>
           </div>
+
           <div className='w-[180px] h-[150px] rounded shadow-md shadow-[#040c16] p-2 m-3'>
             <img className='rounded mx-auto w-[80%] h-[80%]' src={CSS} alt="HTML" />
-            <p className='text-lg uppercase text-center my-2 font-bold'>html</p>
+            <p className='text-lg uppercase text-center my-2 font-bold'>CSS</p>
           </div>
+
           <div className='w-[180px] h-[150px] rounded shadow-md shadow-[#040c16] p-2 m-3'>
             <img className='rounded mx-auto w-[80%] h-[80%]' src={JS} alt="HTML" />
-            <p className='text-lg uppercase text-center my-2 font-bold'>html</p>
+            <p className='text-lg uppercase text-center my-2 font-bold'>Javascript</p>
           </div>
+
           <div className='w-[180px] h-[150px] rounded shadow-md shadow-[#040c16] p-2 m-3'>
-            <img className='rounded mx-auto w-[80%] h-[80%]' src={HTML} alt="HTML" />
-            <p className='text-lg uppercase text-center my-2 font-bold'>html</p>
+            <img className='rounded mx-auto w-[80%] h-[80%]' src={TS} alt="HTML" />
+            <p className='text-lg uppercase text-center my-2 font-bold'>Typescript</p>
           </div>
+
           <div className='w-[180px] h-[150px] rounded shadow-md shadow-[#040c16] p-2 m-3'>
-            <img className='rounded mx-auto w-[80%] h-[80%]' src={HTML} alt="HTML" />
-            <p className='text-lg uppercase text-center my-2 font-bold'>html</p>
+            <img className='rounded mx-auto w-[80%] h-[80%]' src={React} alt="HTML" />
+            <p className='text-lg uppercase text-center my-2 font-bold'>React</p>
           </div>
+
           <div className='w-[180px] h-[150px] rounded shadow-md shadow-[#040c16] p-2 m-3'>
-            <img className='rounded mx-auto w-[80%] h-[80%]' src={HTML} alt="HTML" />
-            <p className='text-lg uppercase text-center my-2 font-bold'>html</p>
+            <img className='rounded mx-auto w-[80%] h-[80%]' src={Node} alt="HTML" />
+            <p className='text-lg uppercase text-center my-2 font-bold'>Node</p>
           </div>
+
           <div className='w-[180px] h-[150px] rounded shadow-md shadow-[#040c16] p-2 m-3'>
-            <img className='rounded mx-auto w-[80%] h-[80%]' src={HTML} alt="HTML" />
-            <p className='text-lg uppercase text-center my-2 font-bold'>html</p>
+            <img className='rounded mx-auto w-[80%] h-[80%]' src={Express} alt="HTML" />
+            <p className='text-lg uppercase text-center my-2 font-bold'>Express</p>
           </div>
+
           <div className='w-[180px] h-[150px] rounded shadow-md shadow-[#040c16] p-2 m-3'>
-            <img className='rounded mx-auto w-[80%] h-[80%]' src={HTML} alt="HTML" />
-            <p className='text-lg uppercase text-center my-2 font-bold'>html</p>
+            <img className='rounded mx-auto w-[80%] h-[80%]' src={Mongo} alt="HTML" />
+            <p className='text-lg uppercase text-center my-2 font-bold'>Mongo DB</p>
+          </div>
+
+          <div className='w-[180px] h-[150px] rounded shadow-md shadow-[#040c16] p-2 m-3'>
+            <img className='rounded mx-auto w-[80%] h-[80%]' src={Tailwind} alt="HTML" />
+            <p className='text-lg uppercase text-center my-2 font-bold'>Tailwind</p>
+          </div>
+
+          <div className='w-[180px] h-[150px] rounded shadow-md shadow-[#040c16] p-2 m-3'>
+            <img className='rounded mx-auto w-[80%] h-[80%]' src={Git} alt="HTML" />
+            <p className='text-lg uppercase text-center my-2 font-bold'>Git</p>
           </div>
           
         </div>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,10 +1,19 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 
 const Work = () => {
   return (
     <div className='h-screen w-full bg-[#0a192f] text-gray-300'>
-        Work
+        <div className='max-w-[1000px] w-full h-full flex flex-col border-2 mx-auto'>
+          <div className='p-4'>
+            <h1 className='text-4xl font-bold border-b-4 border-pink-600 inline rounded'>Work</h1>
+            <p className='mt-4 text-lg'>//These are the projects, I&apos;ve build: </p>
+          </div>
+          <div>
+            
+          </div>
+        </div>
     </div>
   )
 }
