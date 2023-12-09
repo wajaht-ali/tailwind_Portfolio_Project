e046322d2af5cb44f58e49e697539a52d0c8fee7
Wajahat-404 <109886908+Wajahat-404@users.noreply.github.com>
2023-12-09 11:42:33 +0500

    Work component gallery development

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -17,9 +17,20 @@ const Work = () => {
         <div style={{ backgroundImage: `url(${workImg})` }} className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
 
           <div className="shadow-lg shadow-[#040c16] group container flex justify-center items-center mx-auto rounded-md content-div">
-            <div>
-              <span></span>
-              <div></div>
+
+            {/* Hover effect */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                React JS Application
+              </span>
+              <div className='pt-8 text-center'>
+                <a href="/">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold'>Code</button>
+                </a>
+                <a href="/">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold'>Code</button>
+                </a>
+              </div>
             </div>
           </div>
         </div>
