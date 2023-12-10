280ee9113434e3de9f21b33a99efb62b55d1182a
Wajahat-404 <109886908+Wajahat-404@users.noreply.github.com>
2023-12-10 13:11:10 +0500

    Mobile footer added successfully

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,47 +1,50 @@
-import { Link } from "react-scroll";
+// import { Link } from "react-scroll";
 import { HiOutlineMail } from 'react-icons/hi';
-import { FaLinkedin, FaGithub, FaMailBulk } from 'react-icons/fa';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
-
 import img from "../assets/images/portfolioLogNoBg.png";
+
 const Footer = () => {
     return (
-        <div className="">
-            <div name="About-Me">
-                <img src={img} alt="img" style={{ width: '40px' }} />
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. In amet molestiae vel quasi fugiat corrupti voluptas aspernatur earum quo eius.</p>
+        <div className="h-auto w-full bg-[#0a192f] text-gray-300 md:hidden">
+            <div className="max-w-[1000px] w-full h-full grid content-center mx-auto p-4">
+                <div name="About-Me" className="flex flex-col justify-between items-center w-[80%] mx-auto">
+                    <img src={img} alt="img" style={{ width: '60px' }} />
+                    <p className="mt-3 text-justify text-md">Lorem ipsum dolor, sit amet consectetur adipisicing elit. In amet molestiae vel quasi fugiat corrupti voluptas aspernatur earum quo eius.</p>
+                </div>
+                {/* <div name="Nav-items" className="flex justify-center items-center w-full h-full">
+                    <ul className="flex flex-col items-center justify-center h-[160px] p-4">
+                        <li className="p-1 cursor-pointer text-lg border-b-2 border-gray-500">
+                            <Link to={'home'} smooth={true} duration={300}>Home</Link>
+                        </li>
+                        <li className="p-1 cursor-pointer text-lg border-b-2 border-gray-500">
+                            <Link to={'about'} smooth={true} duration={300}>About</Link>
+                        </li>
+                        <li className="p-1 cursor-pointer text-lg border-b-2 border-gray-500">
+                            <Link to={'skills'} smooth={true} duration={300}>Skills</Link>
+                        </li>
+                        <li className="p-1 cursor-pointer text-lg border-b-2 border-gray-500">
+                            <Link to={'work'} smooth={true} duration={300}>Work</Link>
+                        </li>
+                    </ul>
+                </div> */}
             </div>
-            <div name="Nav-items">
-                <ul>
+            <div className="flex flex-row w-full justify-between items-center p-4">
+                <p className="text-md">All rights reserved &copy;</p>
+                <ul className="flex flex-row w-[50%] justify-evenly">
                     <li>
-                        <Link to={'home'} smooth={true} duration={300}>Home</Link>
+                        <a href="https://linkedin.com/in/wajhat-ali/" rel="noreferrer" target="_blank"><FaLinkedin size={20} /></a>
                     </li>
                     <li>
-                        <Link to={'about'} smooth={true} duration={300}>About</Link>
-                    </li>
-                    <li>
-                        <Link to={'skills'} smooth={true} duration={300}>Skills</Link>
-                    </li>
-                    <li>
-                        <Link to={'work'} smooth={true} duration={300}>Work</Link>
-                    </li>
-                </ul>
-            </div>
-            <div name="Social-icons">
-                <ul>
-                    <li>
-                        <a href="https://linkedin.com/in/wajhat-ali/" rel="noreferrer" target="_blank"><FaLinkedin /></a>
-                    </li>
-                    <li>
-                        <a href="https://github.com/wajaht-ali/" rel="noreferrer" target="_blank"><FaGithub /></a>
+                        <a href="https://github.com/wajaht-ali/" rel="noreferrer" target="_blank"><FaGithub size={20} /></a>
                     </li>
                     <li>
                         <abbr title="contact.chwajahat@gmail.com">
-                            <a href="mailto:contact.chwajahat@gmail.com" rel="noreferrer" target="_blank"><HiOutlineMail /></a>
+                            <a href="mailto:contact.chwajahat@gmail.com" rel="noreferrer" target="_blank"><HiOutlineMail size={20} /></a>
                         </abbr>
                     </li>
                     <li>
-                        <a href="" rel="noreferrer" target="_blank"><BsFillPersonLinesFill /></a>
+                        <a href="" rel="noreferrer" target="_blank"><BsFillPersonLinesFill size={20} /></a>
                     </li>
                 </ul>
             </div>
