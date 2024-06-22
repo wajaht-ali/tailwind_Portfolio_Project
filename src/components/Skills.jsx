56ef9892cbaa81ee4fff5a51b53d727e02a6486e
wajaht-ali <contact.chwajahat@gmail.com>
2024-06-22 16:57:34 +0500

    Skill section updated

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -30,60 +30,45 @@ const Skills = () => {
 
         <div className="w-full grid grid-cols-2 md:grid-rows-2 md:grid-cols-4 p-4 mx-auto h-full place-items-center">
 
-          <div className='w-[180px] h-[170px] rounded shadow-md shadow-[#040c16] p-2 m-3 hover:scale-105 duration-200 ease-in-out'>
+          <div className='w-[170px] md:w-[180px] h-[170px] rounded shadow-md shadow-[#040c16] p-2 m-3 hover:scale-105 duration-200 ease-in-out'>
             <img className='rounded mx-auto w-[80%] h-[80%]' src={cpp} alt="HTML" />
             <p className='text-lg uppercase text-center my-2 font-bold'>CPP</p>
           </div>
 
-          {/* <div className='w-[180px] h-[170px] rounded shadow-md shadow-[#040c16] p-2 m-3 hover:scale-105 duration-200 ease-in-out'>
-            <img className='rounded mx-auto w-[80%] h-[80%]' src={CSS} alt="HTML" />
-            <p className='text-lg uppercase text-center my-2 font-bold'>CSS</p>
-          </div> */}
-
-          <div className='w-[180px] h-[170px] rounded shadow-md shadow-[#040c16] p-2 m-3 hover:scale-105 duration-200 ease-in-out'>
+          <div className='w-[170px] md:w-[180px] h-[170px] rounded shadow-md shadow-[#040c16] p-2 m-3 hover:scale-105 duration-200 ease-in-out'>
             <img className='rounded mx-auto w-[80%] h-[80%]' src={JS} alt="HTML" />
             <p className='text-lg uppercase text-center my-2 font-bold'>Javascript</p>
           </div>
 
-          {/* <div className='w-[180px] h-[170px] rounded shadow-md shadow-[#040c16] p-2 m-3 hover:scale-105 duration-200 ease-in-out'>
-            <img className='rounded mx-auto w-[80%] h-[80%]' src={TS} alt="HTML" />
-            <p className='text-lg uppercase text-center my-2 font-bold'>Typescript</p>
-          </div> */}
-
-          <div className='w-[180px] h-[170px] rounded shadow-md shadow-[#040c16] p-2 m-3 hover:scale-105 duration-200 ease-in-out'>
+          <div className='w-[170px] md:w-[180px] h-[170px] rounded shadow-md shadow-[#040c16] p-2 m-3 hover:scale-105 duration-200 ease-in-out'>
             <img className='rounded mx-auto w-[80%] h-[80%]' src={React} alt="HTML" />
             <p className='text-lg uppercase text-center my-2 font-bold'>React</p>
           </div>
 
-          <div className='w-[180px] h-[170px] rounded shadow-md shadow-[#040c16] p-2 m-3 hover:scale-105 duration-200 ease-in-out'>
+          <div className='w-[170px] md:w-[180px] h-[170px] rounded shadow-md shadow-[#040c16] p-2 m-3 hover:scale-105 duration-200 ease-in-out'>
             <img className='rounded mx-auto w-[80%] h-[80%]' src={Node} alt="HTML" />
             <p className='text-lg uppercase text-center my-2 font-bold'>Node</p>
           </div>
 
-          <div className='w-[180px] h-[170px] rounded shadow-md shadow-[#040c16] p-2 m-3 hover:scale-105 duration-200 ease-in-out'>
+          <div className='w-[170px] md:w-[180px] h-[170px] rounded shadow-md shadow-[#040c16] p-2 m-3 hover:scale-105 duration-200 ease-in-out'>
             <img className='rounded mx-auto w-[80%] h-[80%]' src={Express} alt="HTML" />
             <p className='text-lg uppercase text-center my-2 font-bold'>Express</p>
           </div>
 
-          <div className='w-[180px] h-[170px] rounded shadow-md shadow-[#040c16] p-2 m-3 hover:scale-105 duration-200 ease-in-out'>
+          <div className='w-[170px] md:w-[180px] h-[170px] rounded shadow-md shadow-[#040c16] p-2 m-3 hover:scale-105 duration-200 ease-in-out'>
             <img className='rounded mx-auto w-[80%] h-[80%]' src={Mongo} alt="HTML" />
             <p className='text-lg uppercase text-center my-2 font-bold'>Mongo DB</p>
           </div>
 
-          <div className='w-[180px] h-[170px] rounded shadow-md shadow-[#040c16] p-2 m-3 hover:scale-105 duration-200 ease-in-out'>
-            <img className='rounded mx-auto w-[80%] h-[80%]' src={Tailwind} alt="tailwindcss" />
-            <p className='text-lg uppercase text-center my-2 font-bold'>Tailwind CSS</p>
-          </div>
-
-          <div className='w-[180px] h-[150px] rounded shadow-md shadow-[#040c16] p-2 m-3 hover:scale-105 duration-200 ease-in-out'>
+          <div className='w-[170px] md:w-[180px] h-[150px] rounded shadow-md shadow-[#040c16] p-2 m-3 hover:scale-105 duration-200 ease-in-out'>
             <img className='rounded mx-auto w-[80%] h-[80%]' src={git} alt="git" />
             <p className='text-lg uppercase text-center my-2 font-bold'>Git</p>
           </div>
 
-          {/* <div className='w-[180px] h-[150px] rounded shadow-md shadow-[#040c16] p-2 m-3 hover:scale-105 duration-200 ease-in-out'>
-            <img className='rounded mx-auto w-[80%] h-[80%]' src={Git} alt="HTML" />
-            <p className='text-lg uppercase text-center my-2 font-bold'>Git</p>
-          </div> */}
+          <div className='w-[170px] md:w-[180px] h-[170px] rounded shadow-md shadow-[#040c16] p-2 m-3 hover:scale-105 duration-200 ease-in-out'>
+            <img className='rounded mx-auto w-[80%] h-[80%]' src={Tailwind} alt="tailwindcss" />
+            <p className='text-lg uppercase text-center my-2 font-bold'>Tailwind Css</p>
+          </div>
 
         </div>
 
