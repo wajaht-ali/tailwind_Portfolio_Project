56839d8d3345589dc1d061137ded5b6fd4f0ce2b
wajaht-ali <contact.chwajahat@gmail.com>
2025-05-20 22:50:51 +0500

    Feat: Working on experience section

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,8 @@ function App() {
       <Skills />
       <Work />
       <Contact />
-      {/* <Experience />
-      <Certifications /> */}
+      <Experience />
+      {/* <Certifications /> */}
       <Footer />
     </div>
   )
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,10 +1,22 @@
-/* eslint-disable no-unused-vars */
-import React from 'react'
+/* eslint-disable react/no-unescaped-entities */
+// import { Link } from "react-router-dom"
 
 const Experience = () => {
     return (
-        <div>Experience</div>
+        <div name='experience' className="h-screen w-full bg-[#0a192f] text-gray-300">
+            <div className="flex flex-col justify-center items-center w-full h-full px-8">
+                <div className="max-w-[1000px] grid sm:grid-cols-2 gap-8 w-full">
+                    <div className="sm:text-right pb-8 pl-4">
+                        <p className="text-4xl inline font-bold border-b-4 border-blue-500">Experience</p>
+                    </div>
+                    <div></div>
+                </div>
+                <div className="max-w-[1000px] grid sm:grid-cols-2 gap-8 w-full">
+                    
+                </div>
+            </div>
+        </div>
     )
 }
 
-export default Experience
+export default Experience;
